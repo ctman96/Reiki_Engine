a2871e4099ecbe0af0e3d5624c0cb2e208de3e42
Cody Newman <cody.newman@alumni.ubc.ca>
2019-10-30 10:51:26 -0700

    Add GLAD for loading openGL

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-//#include <gl3w.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
 #include "src/Window.hpp"
 
 namespace
@@ -8,6 +9,25 @@ namespace
     void glfw_error(int error, const char* desc)
     {
         fprintf(stderr, "%d: %s", error, desc);
+    }
+
+    // openGl Debug Callback
+    static void APIENTRY openglCallbackFunction(
+            GLenum source,
+            GLenum type,
+            GLuint id,
+            GLenum severity,
+            GLsizei length,
+            const GLchar* message,
+            const void* userParam
+    ){
+        (void)source; (void)type; (void)id;
+        (void)severity; (void)length; (void)userParam;
+        fprintf(stderr, "%s\n", message);
+        if (severity==GL_DEBUG_SEVERITY_HIGH) {
+            fprintf(stderr, "Aborting...\n");
+            abort();
+        }
     }
 }
 
@@ -20,8 +40,18 @@ int main() {
 
     Window window("Test", 800, 600);
 
-    //if (!gl3wInit())
-    //    throw std::runtime_error("Failed to initialize GL3W");
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        throw std::runtime_error("Failed to initialize GLAD!");
+    }
+    printf("OpenGL %d.%d\n", GLVersion.major, GLVersion.minor);
+
+    // Enable the debug callback
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(openglCallbackFunction, nullptr);
+    glDebugMessageControl(
+            GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, true
+    );
 
     glClearColor(0.5f,0.5f,0.5f,0.f);
 
